9ff3f7f7a4cfb6a24e1464b4dce37b18465d6a11
chao <zcsd2668@163.com>
2019-05-31 14:41:48 +0800

    实现按钮事件绑定

---
+++ Assets/Scripts/UI/PanelOptionButtons.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**
+ * 监听选项面板button事件
+ * */
+public class PanelOptionButtons : MonoBehaviour
+{
+    [SerializeField] PauseController pauseController;
+    [SerializeField] GameObject Panel;
+
+    /**
+     * 监听返回游戏button
+     * */
+    public void ReturnButtonPressed()
+    {
+        if (pauseController.Paused)
+        {
+            Panel.gameObject.SetActive(false);
+            pauseController.StartGame();
+            pauseController.Paused = false;
+        }
+    }
+    /**
+     * 监听保存游戏button
+     * */
+    public void SaveButtonPressed()
+    {
+        Debug.Log("SaveButtonPressed");
+    }
+
+    /**
+     * 监听返回菜单button
+     * */
+    public void MenuButtonPressed()
+    {
+        SceneManager.LoadScene("Scenes/StartMenu");
+    }
+
+    /**
+     * 监听返回桌面button
+     * */
+    public void WindowButtonPressed()
+    {
+        Application.Quit();
+    }
+}
